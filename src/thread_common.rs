37ae435524abfe5c2febe4a60a6f83c4205682bc
somedevfox [Fedora] <somedevfox@gmail.com>
2022-04-24 08:51:03 -0400

    We do a little bit of documentation

--- src/thread_common.rs
+++ src/thread_common.rs
@@ -12,10 +12,72 @@ pub enum RGSSError {
 }
 
 pub enum MessageTypes {
+    /*
+    * RGSSThreadTerminate
+    * > When thread encounters any exception (such as uninitialized constant or Scripts.rxdata not found)
+    * > It will print out error, then send a message with enum of RGSSThreadTerminate which has an argument 
+    * > of RGSSError (see enum above)
+    * > rcxp_window.rs will receive RGSSThreadTerminate enum and will simply close window.
+    * 
+    * Arguments:
+    *     RGSSThread - error
+    */
     RGSSThreadTerminate(RGSSError),
+
+    /* 
+    * BitmapCreate
+    * > Creates SFML Texture and stores it in bitmap_ids HashMap in rcxp_window.rs.
+    * > Does not return anything to sender. // TODO: send some kind of result back to sender for creation verification
+    * 
+    * Arguments:
+    *     1st u32 - width
+    *     2nd u32 - height
+    *     u64     - bitmap id
+    */
     BitmapCreate(u32, u32, u64),
+
+    /* 
+    * BitmapDispose
+    * > Deletes SFML Texture and removes it from bitmap_ids HashMap in rcxp_window.rs.
+    * > If Bitmap has been already disposed of, will do nothing.
+    * > Does not return anything to sender.
+    * 
+    * Arguments:
+    *     u64 - bitmap id
+    */
     BitmapDispose(u64),
+
+    /*
+    * BitmapCheckIfDisposed
+    * > Checks if bitmap exists in bitmap_ids HashMap in rcxp_window.rs,
+    * > Then, returns BitmapCheckIfDisposedResult enum to sender.
+    *
+    * BitmapCheckIfDisposed Arguments:
+    *     u64  - bitmap id
+    * BitmapCheckIfDisposedResult Arguments:
+    *     bool - bool which tells whether bitmap exists or not
+    */
+    BitmapCheckIfDisposed(u64),
+    BitmapCheckIfDisposedResult(bool),
+
+    /*
+    * SpriteCreate
+    * > Creates SFML Sprite and stores it in sprite_ids HashMap in rcxp_window.rs.
+    * > Does not return anything to sender. // TODO: send some kind of result back to sender for creation verification
+    * Arguments:
+    *     u64 - sprite id
+    */
     SpriteCreate(u64),
+
+    /* 
+    * SpriteDispose
+    * > Deletes SFML Sprite and removes it from sprite_ids HashMap in rcxp_window.rs.
+    * > If Sprite has been already disposed of, will do nothing.
+    * > Does not return anything to sender.
+    * 
+    * Arguments:
+    *     u64 - sprite id
+    */
     SpriteDispose(u64)
 }
 
